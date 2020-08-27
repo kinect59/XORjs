f8bb62009fd5e28f6aae59d898183b283a503709
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-27 23:59:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,32 @@
-/**
 
-// Tiny TFJS train / predict example.
-async function run() {
-  // Create a simple model.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+var tf = require('@tensorflow/tfjs');
 
-  // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+async function train_test() {
 
-  // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 250});
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 10, activation: 'sigmoid',inputShape: [2]}));
+model.add(tf.layers.dense({units: 1, activation: 'sigmoid',inputShape: [10]}));
 
-  // Use the model to do inference on a data point the model hasn't seen.
-  // Should print approximately 39.
-  document.getElementById('micro-out-div').innerText =
-      model.predict(tf.tensor2d([20], [1, 1])).dataSync();
+model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
+
+const training_data = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
+const target_data = tf.tensor2d([[0],[1],[1],[0]]);
+
+for (let i = 1; i < 100 ; ++i) {
+ var h = await model.fit(training_data, target_data, {epochs: 30});
+   console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
 }
 
-run();
+ model.predict(training_data).print();
 
- */
+}
 
+train_test();
 
 /*
 // Tiny TFJS train / predict example.
-async function run() {
-  // Create a simple model.
-  const model = tf.sequential();
-  model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-  // Prepare the model for training: Specify the loss and the optimizer.
-  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-  // Generate some synthetic data for training. (y = 2x - 1)
-  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-
-  // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 250});
-
-  // Use the model to do inference on a data point the model hasn't seen.
-  // Should print approximately 39.
-  document.getElementById('micro-out-div').innerText =
-      model.predict(tf.tensor2d([20], [1, 1])).dataSync();
-}
-
-run();
-
- */
-
-/*tf.loadModel('file://./model-1a/model.json').then(model => {
-  model.predict();
-});*/
-
-
-// Tiny TFJS train / predict example.
-async function run() {
+async function laod_and_test() {
  
   model = await tf.loadModel('XOR/web_model/model.json').then(model => {
   model.predict();
@@ -72,4 +38,5 @@ async function run() {
   
 }
 
-run();
+laod_and_test();
+*/
