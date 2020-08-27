c45c1926e7e8cf685aafd3373200d752eb7bb390
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-28 00:17:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,26 @@ async function run() {
 run();
 */
 
-
+async function run() {
+  // Create a simple model.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({units: 10, activation: 'sigmoid',inputShape: [2]}));
+  model.add(tf.layers.dense({units: 1, activation: 'sigmoid',inputShape: [10]}));
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+  //model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
+  // Generate some synthetic data for training. (y = 2x - 1)
+  const xs = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
+  const ys = tf.tensor2d([[0],[1],[1],[0]]);
+  // Train the model using the data.
+  await model.fit(xs, ys, {epochs: 250});
+  // Use the model to do inference on a data point the model hasn't seen.
+  // Should print approximately 39.
+  document.getElementById('micro-out-div').innerText =
+      model.predict(tf.tensor2d([1, 0])).dataSync();
+}
+run();
+/*
 //var tf = require('@tensorflow/tfjs');
 
 async function train_test() {
@@ -32,23 +51,18 @@ model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
 const training_data = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
 const target_data = tf.tensor2d([[0],[1],[1],[0]]);
 
-await model.fit(xs, ys, {epochs: 250});
-  // Use the model to do inference on a data point the model hasn't seen.
-  // Should print approximately 39.
-document.getElementById('micro-out-div').innerText = model.predict(tf.tensor2d([0,1])).dataSync();
-  /*
 for (let i = 1; i < 100 ; ++i) {
  var h = await model.fit(training_data, target_data, {epochs: 30});
    console.log("Loss after Epoch " + i + " : " + h.history.loss[0]);
 }
 
  model.predict(training_data).print();
- */
+
 
 }
 
 train_test();
-
+*/
 
 /*
 // Tiny TFJS train / predict example.
