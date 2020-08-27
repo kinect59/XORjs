1140ceb9bc0c5cb3a88a38e385a589e7dd37757d
kinect59 <hazem.wannous@univ-lille1.fr>
2020-08-28 00:21:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ async function run() {
   //model.compile({loss: 'meanSquaredError', optimizer: 'rmsprop'});
   // Generate some synthetic data for training. (y = 2x - 1)
   const xs = tf.tensor2d([[0,0],[0,1],[1,0],[1,1]]);
-  const ys = tf.tensor2d([[0],[1],[1],[0]]);
+  const ys = tf.tensor2d([0,1,1,0]);
   // Train the model using the data.
-  await model.fit(xs, ys, {epochs: 10});
+  await model.fit(xs, ys, {epochs: 250});
   // Use the model to do inference on a data point the model hasn't seen.
   // Should print approximately 39.
   document.getElementById('micro-out-div').innerText =
